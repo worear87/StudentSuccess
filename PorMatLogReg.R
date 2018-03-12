78fa53d89e6e5e0ed77ff65ab026dbeaa76ebea2
Wallace O'Rear <wallace.c.o'rear@boeing.com>
2018-03-12 16:10:31 -0400

    did some logistic stuff and added the data files

--- PorMatLogReg.R
+++ PorMatLogReg.R
@@ -3,9 +3,9 @@ library(data.table)
 
 #bring in data
 #math class
-student.mat <- data.table(read.csv("~/StudentSuccess/student/student-mat.csv", sep=";"))
+student.mat <- data.table(read.csv("student-mat.csv", sep=";"))
 #portugese class
-student.por <- data.table(read.csv("~/StudentSuccess/student/student-por.csv", sep=";"))
+student.por <- data.table(read.csv("student-por.csv", sep=";"))
 
 ## G1 = first period grade, G2 = second period grade, G3 = final grade
 
@@ -13,10 +13,11 @@ student.por <- data.table(read.csv("~/StudentSuccess/student/student-por.csv", s
 student.mat <- student.mat[,Success:=ifelse(G3 >= 14, 1,0)]
 student.por <- student.por[,Success:=ifelse(G3 >= 14, 1,0)]
 
+student.mat <- data.frame(sapply(student.mat,as.factor))
 
 ## set train and test set for math class
-student.mat.train <- student.mat[1:200,]
-student.mat.test  <- student.mat[201:395,]
+student.mat.train <- student.mat[1:350,]
+student.mat.test  <- student.mat[301:395,]
 
 ## set train and test set for portugese class
 student.por.train <- student.por[1:325,]
@@ -24,8 +25,13 @@ student.por.test  <- student.por[326:649,]
 
 ### first model is without any grades
 
-matModelNoGrades <- glm(Success ~ . - G1 - G2 - G3, family = binomial(link = "logit"), student.mat.train)
+matModelNoGrades <- glm(Success ~ . , family = binomial(link = "logit"), data =  subset(student.mat.train, select = -c(G1,G2,G3)))
+
+matModelNoGrades2 <- glm(Success ~ Medu + paid + absences + reason + failures + Dalc, family=binomial, data = student.mat.train)
 
 ### second model is with G1 and qualitative data
+matModelWG1 <- glm(Success ~ . , family = binomial(link = "logit"), data =  subset(student.mat.train, select = -c(G2,G3)))
 
 ### third model is with G1 and G2 and qualitative data
+
+matModelWG1G2 <- glm(Success ~ . -G3, family = binomial(link = "logit"), data =  student.mat.train)
